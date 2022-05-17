5af09152dec2f32be111267f21044070b60b1a9c
Uchiha Kim Jun Hyung <105705424+Goodyera-K@users.noreply.github.com>
2022-05-17 16:47:40 +0900

    Create xhan_labs_special3.c

---
+++ xhan_labs_special3.c
@@ -0,0 +1,13 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
+#include <stdio.h>
+int main() {
+	char arr[];
+	for (int i = 0; i < 8 < i++) {
+		scanf(" %c", &arr[i]);
+
+	}
+	for (int j = 0; j < 8; j++) {
+		printf("%c", arr[j]);
+	}
+}
